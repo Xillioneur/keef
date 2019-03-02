ff3a7d9762661cc9d8d8d3672cb0233448a82eac
SlickJohnson <liwa.johnson@gmail.com>
2019-03-02 10:59:54 -0800

    Add update route and update user model params

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -18,4 +18,22 @@ final class UserController {
             }
         }
     }
+
+  // update an existing user
+  func update(_ req: Request) throws -> Future<Response> {
+    return try req.parameters.next(User.self).flatMap { user in
+      return try req.content.decode(UserForm.self).flatMap { userForm in
+        user.username = userForm.username
+        return user.save(on: req).map { _ in
+          return req.redirect(to: "/users")
+        }
+      }
+    }
+  }
 }
+
+// Form used to decode user for update
+struct UserForm: Content {
+  var username: String
+}
+
