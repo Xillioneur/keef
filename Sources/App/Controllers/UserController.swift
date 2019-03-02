9d364f1a68283639335e7ddd8039773a9c9f12fd
SlickJohnson <liwa.johnson@gmail.com>
2019-03-02 10:01:12 -0800

    Create crud leaf

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,7 +6,7 @@ final class UserController {
     func list(_ req: Request) throws -> Future<View> {
         return User.query(on: req).all().flatMap { users in
             let data = ["userlist": users]
-            return try req.view().render("userview", data)
+            return try req.view().render("crud", data)
         }
     }
 
